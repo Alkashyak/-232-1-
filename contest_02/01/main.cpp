657c3cf5b04ea2baaf24e83e4b21ab0fd28fcd61
Alkashyak <134154173+Alkashyak@users.noreply.github.com>
2024-01-01 21:43:18 +0300

    Update main.cpp

--- contest_02/01/main.cpp
+++ contest_02/01/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
-// Ваш код будет вставлен сюда
+#include <cmath>
+bool istina = true;
+int is_prime(long n) {
+    for(long i = 2; i < (pow(n, 0.5)+1); i++) {
+        if(n % i == 0){istina = false;}
+    }
+return int(istina);
+}
 
 int main(){
     int number;
