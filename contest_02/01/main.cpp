6d007c270f81a437712e06e7c7e2245779c64f13
Alkashyak <134154173+Alkashyak@users.noreply.github.com>
2024-01-01 21:35:14 +0300

    Create main.cpp

---
+++ contest_02/01/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include <cmath>
+bool istina = true;
+int is_prime(long n) {
+    for(long i = 2; i < (pow(n, 0.5)+1); i++) {
+        if(n % i == 0){istina = false;}
+    }
+return int(istina);
+}
+int main(){
+    int number;
+    std::cin >> number;
+    std::cout << (is_prime(number) ? "YES" : "NO") << std::endl;
+}
